497099fba959b637c0650982898f099c8f5233f1
Avinashreddy18 <63732732+Avinashreddy18@users.noreply.github.com>
2021-06-12 19:13:24 +0530

    Add files via upload

---
+++ s.js
@@ -0,0 +1,12 @@
+var l=["hello","john","kiran","jagan","Jar","ravi","kiran","tharun"];
+for(var i=0;i<l.length;i++)
+{
+   if(l[i].charAt(0)=="j"||l[i].charAt(0)=="J")
+   {
+    console.log("Goodye"+" " +l[i]);
+   }
+   else
+  {
+   console.log("Hello"+" "+ l[i]);
+}
+}
